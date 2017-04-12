f8ac644fbf30901879c419f3dc4f6ed0e7afa5fa
Max Van Lyl <maximvanlyl@gmail.com>
2017-04-12 14:10:25 -0400

    initial commit

---
+++ Reading Docs 2/optional_arguments_redux.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+puts Date.new
+puts Date.new(2016)
+puts Date.new(2016, 5)
+puts Date.new(2016, 5, 13)
+
+# These will print:
+# current date.
+# january 1st 2016
+# May 1st 2016
+# May 13th 2016
